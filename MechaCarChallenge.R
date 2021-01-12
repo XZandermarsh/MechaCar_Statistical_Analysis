69381d92b541d0da605e7ab57f700d6997c4db64
Alex Felice <golfzander@comcast.net>
2021-01-12 11:06:35 -0600

    Suspension Coils Rscript and screenshots added

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Statistical Summary of LR
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_data))
+
+# Import suspension coil data from csv file
+coil_data <- read.csv('Suspension_Coil.csv', stringsAsFactors = F)
+
+# Summarize overall statistics of coil PSI
+total_summary <- coil_data %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), variance_PSI=var(PSI), sd_PSI=sd(PSI), .groups = 'keep')
+
+# Summarize lot statistics of coil PSI
+lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), variance_PSI=var(PSI), sd_PSI=sd(PSI), .groups = 'keep')
+
+
+
