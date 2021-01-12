0c21d141e82e37714f1c2482d9c89c1f835643c7
Alex Felice <golfzander@comcast.net>
2021-01-12 12:36:42 -0600

    t-test RScript and Screenshot

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,5 +19,16 @@ total_summary <- coil_data %>% summarize(mean_PSI=mean(PSI), median_PSI=median(P
 # Summarize lot statistics of coil PSI
 lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>% summarize(mean_PSI=mean(PSI), median_PSI=median(PSI), variance_PSI=var(PSI), sd_PSI=sd(PSI), .groups = 'keep')
 
+# One sample t-test to see if overall PSI is statistically different from assumed population mean of 1500 PSI
+t.test(coil_data$PSI, mu=1500)
 
+# Create new dataframes for each mfg lot to compare in t-test
+coil_lot1 <- coil_data %>% filter(Manufacturing_Lot=='Lot1')
+coil_lot2 <- coil_data %>% filter(Manufacturing_Lot=='Lot2')
+coil_lot3 <- coil_data %>% filter(Manufacturing_Lot=='Lot3')
+
+# Perform t-tests on each lot to see if there is a statistical variance from assumed population mean of 1500 PSI
+t.test(coil_lot1$PSI, mu=1500)
+t.test(coil_lot2$PSI, mu=1500)
+t.test(coil_lot3$PSI, mu=1500)
 
